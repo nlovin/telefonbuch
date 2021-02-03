55a3e21e52493458fa861ab65c30699fa3f73ff4
Nathan Lovin <natelovin@gmail.com>
2021-02-03 13:43:13 -0500

    made a shorter book

--- buch.py
+++ buch.py
@@ -1,12 +1,12 @@
 from gtts import gTTS
 import os
 
-fh = open("telefonbuch.txt", "r")
+fh = open("telefonbuch2.txt", "r")
 buch = fh.read().replace("\n", " ")
 
 language = 'en'
 
-output = gTTS(text=buch, lang=language, slow=True)
+output = gTTS(text=buch, lang=language, slow=False)
 
 output.save("telefonbuchleser.mp3")
 fh.close()
